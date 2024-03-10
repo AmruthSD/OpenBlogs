26a7fda19140028878b59cf687d758e081a33a5c
aahilrafiq <aahil.rafiq136@gmail.com>
2024-03-10 20:29:56 +0530

    backend auth complete

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,61 @@
+import { createNewUserSQL , getUserSQL } from "../sql/queries/user.js";
+import jwt from 'jsonwebtoken'
+import 'dotenv/config'
+
+export async function createNewUser(connection , req , res) {
+    const { username , password } = req.body
+    try{
+        const [rows , fields] = await getUserSQL(connection, username)
+        if(rows.length > 0) {
+            return res.status(400).json({message : 'Username already exists'})
+        }
+        await createNewUserSQL(connection, username, password)
+        const [rows2 , fields2] = await getUserSQL(connection, username , password)
+        const userinfo = rows2[0]
+        const tokenpayload = {
+            id : userinfo.id,
+            username : userinfo.username
+        }
+        const token = jwt.sign(tokenpayload , process.env.JWT_SECRET , {expiresIn : '10d'})
+        res.status(200).json({authtoken : token , userdata : tokenpayload})
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({message : 'Internal Server Error'})
+    }
+}
+
+export async function loginUser(connection , req , res) {
+    const { username , password } = req.body
+    try{
+        const [rows , fields] = await getUserSQL(connection, username , password)
+        if(rows.length === 0) {
+            return res.status(401).json({message : 'Unauthorized'})
+        }
+        const userinfo = rows[0]
+        const tokenpayload = {
+            id : userinfo.id,
+            username : userinfo.username
+        }
+        const token = jwt.sign(tokenpayload , process.env.JWT_SECRET , {expiresIn : '10d'})
+        res.status(200).json({authtoken : token})
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({message : 'Internal Server Error'})
+    }
+}
+
+export async function authmiddleware(req , res , next) {
+    const token = req.headers.authorization ? req.headers.authorization.split(' ')[1] : null
+    if(!token) {
+        return res.status(401).json({message : 'Unauthorized'})
+    }
+    try{
+        const decoded = jwt.verify(token , process.env.JWT_SECRET)
+        if(!decoded) {
+            return res.status(401).json({message : 'Unauthorized'})
+        }
+        next()
+    } catch(err) {
+        return res.status(401).json({message : 'Unauthorized'})
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,40 @@
 import 'dotenv/config'
 import express from 'express'
 import mysql from 'mysql2/promise'
+import cors from 'cors'
 const app = express()
 const port = process.env.PORT || 5000
+import { createNewUser , loginUser , authmiddleware } from './controllers/userControllers.js'
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Middleware
+app.use(cors(
+    {
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true
+    }
+));
 
 const connection = await mysql.createConnection({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     database: process.env.MYSQL_DATABASE,
     password: process.env.MYSQL_PASSWORD
+})
+
+app.post('/users/createnewuser', async (req, res) => {
+    await createNewUser(connection, req, res)
+})
+
+app.post('/users/login', async (req, res) => {
+    await loginUser(connection, req, res)
+})
+
+app.get('/users/authmiddleware', authmiddleware, (req, res) => {
+    res.status(200).json({message : 'Authorized'})
 })
 
 app.listen(port, () => {
