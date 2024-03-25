4f5755a7196fcf073518780a14d8d6bf933a86eb
aahilrafiq <aahil.rafiq136@gmail.com>
2024-03-25 11:38:26 +0530

    removed nextui

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,24 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { NextUIProvider } from "@nextui-org/react";
-import {
-  ThemeProvider as NextThemesProvider,
-  ThemeProvider,
-} from "next-themes";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <ThemeProvider attribute="class" defaultTheme="dark">
         <BrowserRouter>
           <main className="font-sans">
             <App />
           </main>
         </BrowserRouter>
-      </ThemeProvider>
-    </NextUIProvider>
   </React.StrictMode>
 );
